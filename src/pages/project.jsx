6ed161b02e490557ec1d5791a5a14e490a147fda
firleichjordan <jordanhombing.js@gmail.com>
2023-11-27 14:17:34 +0000

    change route all image again

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -13,7 +13,7 @@ const Project = () => {
     return (
         <div className="project-page">
             <div className='background-card'>
-                    <img className="project-image" src="/public/bookstores.png" alt="bookstores"/>
+                    <img className="project-image" src="bookstores.png" alt="bookstores"/>
                     <div className='project-info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam atque quisquam esse nostrum dolorem delectus doloremque sapiente incidunt eius eligendi ea praesentium necessitatibus corporis, consequatur aliquam non optio quod cumque quis illum sit. Tempore dolorum et cumque repellat cupiditate animi ex ab voluptates nulla aliquam vel exercitationem praesentium, earum itaque quae minus veniam quidem impedit? Tenetur, inventore cumque aliquid aperiam quis vel modi ipsum ut dicta autem, qui explicabo? Suscipit iste dolores alias praesentium excepturi modi, eligendi quam error sit sequi. Illo commodi tenetur atque dolorem ex voluptatem omnis possimus. Dolor ducimus, non iure repudiandae culpa molestiae optio rem? Soluta.</div>
             </div>
             <div className='btnin-project'>
