65614854b2ad971b3d45ae33c54b49a78fd7884a
firleichjordan <jordanhombing.js@gmail.com>
2023-11-27 14:07:05 +0000

    png to jpg

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -37,8 +37,8 @@ const Profile = () => {
                         <div className="tech">
                             <h3>Tech Back-End</h3>
                             <div className="icon">
-                                <img src="/public/nodejs-48.png" alt="node-js-48"/>
-                                <img src="/public/express-js-48.png" alt="express-js-48"/>
+                                <img src="/public/express-js-48.jpg" alt="express-js-48"/>
+                                <img src="/public/nodejs-48.jpg" alt="node-js-48"/>
                                 <img src="/public/mysql-48.png" alt="mysql-48"/>
                             </div>
                         </div>
